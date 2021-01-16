9f9186a2f374a2abc8c4a55755d30f0f2ce2e1af
Kacper Kipa <51452942+Kicper@users.noreply.github.com>
2021-01-16 12:35:46 +0100

    Add files via upload

--- aco.py
+++ aco.py
@@ -1,5 +1,6 @@
 import math
 import networkx as nx
+import random
 from typing import List
 from ant import Ant
 
@@ -10,14 +11,21 @@ from ant import Ant
 def ant_colony_optimization(graph: nx.Graph, source, destination, m, alpha, beta, starting_pheromone_level, evaporation_coeff, iters):
     # pheromone level and ants init
     graph, ants = init_environment(graph, m, starting_pheromone_level)
+    print_pheromone(graph)
     current_best_solution = []
     current_best_distance = math.inf
     # or while convergence_met(): # todo
     for i in range(iters):
         ants, ants_in_destination = reset_ants(ants)
-        while ants_in_destination != m:
+        # array of lost ants
+        ants_lost = []
+        # until every ant reach destination:
+        while ants_in_destination + len(ants_lost) != m:
             # update_pheromones
+            iter = 0
+            
             for ant in ants:
+                iter = iter + 1
                 if not ant.has_reached_destination():
                     # visit new vertex
                     # if ant starting from source
@@ -25,9 +33,20 @@ def ant_colony_optimization(graph: nx.Graph, source, destination, m, alpha, beta
                         ant.set_visited(source)
                     current_vertex = ant.get_visited_vertices()[-1]
                     adjacent_data = get_allowed_vertices_data(graph, current_vertex, ant.get_visited_vertices())
-                    next_vertex = pick_next_vertex(alpha, beta, current_vertex, adjacent_data)
+
+
+                    if not adjacent_data:
+                        if ant not in ants_lost:
+                            ants_lost.append(ant)
+                        continue
+                    if(i >= 3):
+                        next_vertex = pick_next_vertex(alpha, beta, current_vertex, adjacent_data)
+                    else:
+                        next_vertex = pick_next_vertex_random(alpha, beta, current_vertex, adjacent_data)
                     # print(f'ant {ants.index(ant)}\nnext vertex: {next_vertex}\n')
                     ant.set_visited(next_vertex)
+                    if adjacent_data:
+                        print_everything(graph, ant, iter, i)
                     # if ant has reached destination
                     if next_vertex == destination:
                         ant.reach_destination()
@@ -35,13 +54,29 @@ def ant_colony_optimization(graph: nx.Graph, source, destination, m, alpha, beta
                         solution = ant.get_visited_vertices()
                         solution_length = get_path_length(graph, solution)
                         current_best_solution, current_best_distance = set_best_solution(current_best_solution,
-                                                                                         current_best_distance,
-                                                                                         solution, solution_length)
+                                                                                            current_best_distance,
+                                                                                            solution, solution_length)
                         # print(f'Ant {ants.index(ant)}, solution: {solution}, and best: {current_best_solution}\n')
-            local_pheromone_update(graph, ants, starting_pheromone_level, evaporation_coeff)
+            local_pheromone_update(graph, ants, starting_pheromone_level, evaporation_coeff, ants_lost)
         # global_pheromone_update(graph) # todo
     solution = current_best_solution
+    print_pheromone(graph)
     return solution
+
+
+def print_everything(g: nx.Graph, ant, iter, i):
+    print("Which ant:", iter)
+    print("Main loop:", i)
+    print("Actual road:", ant.get_visited_vertices())
+    print(ant.get_visited_vertices()[-2])
+    print(ant.get_visited_vertices()[-1])
+    print(g[ant.get_visited_vertices()[-2]][ant.get_visited_vertices()[-1]]['pheromone'])
+    print("")
+
+
+def print_pheromone(graph: nx.Graph):
+    for e in graph.edges:
+        print(graph.edges[e]['pheromone'])
 
 
 def get_allowed_vertices_data(graph: nx.Graph, source, visited):
@@ -64,11 +99,33 @@ def pick_next_vertex(alpha, beta, current, adj_data):
     return next_vertex
 
 
+def pick_next_vertex_random(alpha, beta, current, adj_data):
+    highest_probability = 0.0
+    next_vertex = current
+
+    rand = random.randint(0, 3)
+    if(rand < 1):
+        rand_vertex = random.randint(1, len(adj_data))
+        i = 0
+        for vertex in adj_data:
+            i = i + 1
+            if(i == rand_vertex):
+                next_vertex = vertex
+                return next_vertex
+    else:
+        for vertex in adj_data:
+            vertex_prob = get_vertex_probability(vertex, adj_data, alpha, beta)
+            if vertex_prob > highest_probability:
+                highest_probability = vertex_prob
+                next_vertex = vertex
+        return next_vertex
+
+
 def get_vertex_probability(destination, edges_data, alpha, beta):
-    probability_numerator = edges_data[destination]['pheromone']**(alpha) * (1.0 / edges_data[destination]['weight'])**(beta)
+    probability_numerator = edges_data[destination]['pheromone']**(alpha) * (1.0 / edges_data[destination]['cost'])**(beta)
     denominator_data = {}
     for end_vertex, edge in edges_data.items():
-        denominator_data[end_vertex] = edge['pheromone'] **(alpha) * (1.0 / edge['weight'])**(beta)
+        denominator_data[end_vertex] = edge['pheromone'] **(alpha) * (1.0 / edge['cost'])**(beta)
     probability_denominator = sum(denominator_data.values())
     choice_probability = probability_numerator / probability_denominator
     return choice_probability
@@ -77,7 +134,7 @@ def get_vertex_probability(destination, edges_data, alpha, beta):
 def get_path_length(graph: nx.Graph, path):
     distance = 0.0
     for vertex, next_vertex in zip(path, path[1:]):
-        edge_length = graph.adj[vertex][next_vertex]['weight']
+        edge_length = graph.adj[vertex][next_vertex]['cost']
         distance += edge_length
     return distance
 
@@ -102,11 +159,15 @@ def reset_ants(ants: List[Ant]):
     return ants, ants_in_destination
 
 
-def local_pheromone_update(graph: nx.Graph, ants: List[Ant], starting_pheromone, evaporation):
+def local_pheromone_update(graph: nx.Graph, ants: List[Ant], starting_pheromone, evaporation, ants_lost):
     for ant in ants:
-        latest_edge_vertices = (ant.get_visited_vertices()[-2], ant.get_visited_vertices()[-1])
-        latest_edge = graph.adj[latest_edge_vertices[0]][latest_edge_vertices[1]]
-        latest_edge['pheromone'] = (1.0 - evaporation)*latest_edge['pheromone'] + evaporation*starting_pheromone
+        if(ant not in ants_lost):
+            for i in range(len(ant.get_visited_vertices())-1):
+                
+                last_pheromone_level = graph[ant.get_visited_vertices()[i]][ant.get_visited_vertices()[i+1]]['pheromone']
+                amount_of_pheromone = 20/len(ant.get_visited_vertices())
+                new_pheromone_level = (1.0 - evaporation)*last_pheromone_level + evaporation*amount_of_pheromone
+                graph[ant.get_visited_vertices()[i]][ant.get_visited_vertices()[i+1]]['pheromone'] = new_pheromone_level
     return
 
 
--- main.py
+++ main.py
@@ -2,28 +2,49 @@ import networkx as nx
 import math
 from aco import ant_colony_optimization
 
-# testing graph
+
 G = nx.Graph()
-G.add_nodes_from([(3, 8), (10, 7), (10, 3), (8, 1), (4, 3), (7, 6)])
-G.add_edges_from([((3, 8), (10, 7)), ((3, 8), (7, 6)), ((3, 8), (4, 3)),
-                  ((10, 7), (7, 6)), ((10, 7), (10, 3)),
-                  ((10, 3), (4, 3)), ((10, 3), (8, 1)),
-                  ((8, 1), (4, 3)),
-                  ((4, 3), (7, 6))])
-for e in G.edges:
-    # euclidean distance
-    weight = math.sqrt((e[0][0] - e[1][0])**2 + (e[0][1] - e[1][1])**2)
-    G.edges[e]['weight'] = weight
-source_coords = (7, 6)
-destination_coords = (8, 1)
 
-ants_number = 3
-alpha = 1.0
-beta = 1.0
-pheromone_level = 1.0
-max_iters = 10
-pheromone_evaporation = 0.1
 
-solution = ant_colony_optimization(G, source_coords, destination_coords, ants_number, alpha, beta, pheromone_level, pheromone_evaporation, max_iters)
-print(solution)
+def read_file():
+    with open( 'input.txt', 'r' ) as file:
+        line = file.readline()
+        while( "NODES" not in line ):
+            line = file.readline()
+        line = file.readline()
+        splitted = line.split()
+        while( ")" not in splitted[0] ):
+            G.add_node(splitted[0], longitude=float(splitted[2]), latitude=float(splitted[3]), real_cost=float('inf'), tentative_cost=float('inf'), prev_node="")
+            line = file.readline()
+            splitted = line.split()
+        while( "LINKS" not in line ):
+            line = file.readline()
+        line = file.readline()
+        splitted = line.split()
+        while( ")" not in splitted[0] ):
+            G.add_edge(splitted[2], splitted[3], cost=float(splitted[11]))
+            line = file.readline()
+            splitted = line.split()
 
+
+def ant_algorithm():
+    
+    ants_number = 10
+    alpha = 1.0
+    beta = 1.0
+    pheromone_level = 1.0
+    max_iters = 10
+    pheromone_evaporation = 0.1
+    source_coords, destination_coords = "Vancouver", "Houston"
+
+    solution = ant_colony_optimization(G, source_coords, destination_coords, ants_number, alpha, beta, pheromone_level, pheromone_evaporation, max_iters)
+    print("Solution: ")
+    print(solution)
+
+
+def main():
+    read_file()
+    ant_algorithm()
+
+
+main()
